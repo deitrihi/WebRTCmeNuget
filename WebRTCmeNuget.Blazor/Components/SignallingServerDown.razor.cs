3e30738bdb32215a421013ad9fea7a178c184688
Deitrihi <deitrihi@naver.com>
2021-08-14 02:42:16 +0900

    WebRTCme.Api.dll import

---
+++ WebRTCmeNuget.Blazor/Components/SignallingServerDown.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRTCmeNuget.Blazor.Components
+{
+    public partial class SignallingServerDown
+    {
+        private void OnOk()
+        {
+        }
+    }
+}
